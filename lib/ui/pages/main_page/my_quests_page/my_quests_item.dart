fad44a33a603fd40d03adb3db014f530451c3a8b
bakhanetssimon <Bakhanetssimon@gmail.com>
2021-05-18 19:48:55 +0700

    my_quests_item.dart refactored

---
+++ lib/ui/pages/main_page/my_quests_page/my_quests_item.dart
@@ -0,0 +1,193 @@
+import 'dart:async';
+
+import 'package:app/ui/widgets/priority_view.dart';
+import 'package:app/work_quest_app.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../enums.dart';
+
+class MyQuestsItem extends StatelessWidget {
+  const MyQuestsItem({
+    required this.itemType,
+    required this.priority,
+    required this.price,
+    this.creatorName = "Samantha Sparcs",
+    this.imageURL =
+        "https://i.pinimg.com/736x/a9/3c/b4/a93cb4e0316ef9c4db83846550ff4deb.jpg",
+    this.distance = 150,
+  });
+
+  final QuestItemPriorityType itemType;
+  final int priority;
+  final String price;
+  final String creatorName;
+  final String imageURL;
+  final int distance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      margin: const EdgeInsets.only(
+        top: 10,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          getQuestHeader(itemType),
+          Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(37),
+                child: Image.network(
+                  imageURL,
+                  width: 30,
+                  height: 30,
+                ),
+              ),
+              Text(
+                creatorName,
+                style: TextStyle(fontSize: 16),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 17.5,
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.location_on_rounded,
+                color: Color(0xFF7C838D),
+              ),
+              SizedBox(
+                width: 9,
+              ),
+              Text(
+                "$distance from you",
+                style: TextStyle(color: Color(0xFF7C838D)),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            "Paint the gaarage quickly",
+            style: TextStyle(
+              color: Color(0xFF1D2127),
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            "Hi, i’m urgently looking for a skilled man that can paint my Garage doors and a couple of walls around the garage and by the way...",
+            style: TextStyle(
+              color: Color(0xFF4C5767),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: [
+              PriorityView(priority),
+              Spacer(),
+              Text(
+                price,
+                style: TextStyle(
+                    color: Color(0xFF00AA5B),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget getQuestHeader(QuestItemPriorityType itemType) {
+    Widget returnWidget = Container();
+    switch(itemType){
+      case QuestItemPriorityType.Active:
+        returnWidget = Container(
+          margin: const EdgeInsets.symmetric(vertical: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 7.5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: AppColors.green,
+          ),
+          child: Row(
+            children: [
+              Text(
+                "Active",
+                style: TextStyle(color: Colors.white),
+              ),
+              Spacer(),
+              Text(
+                "Runtime",
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                "14:10:23",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+        );
+        break;
+      case QuestItemPriorityType.Invited:
+        returnWidget = Container(
+          margin: const EdgeInsets.symmetric(vertical: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 7.5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: Color(0xFFE8D20D),
+          ),
+          child: Row(
+            children: [
+              Text(
+                "You invited",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+        );
+        break;
+      case QuestItemPriorityType.Performed:
+        returnWidget = Container(
+          margin: const EdgeInsets.symmetric(vertical: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 7.5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: Color(0xFF0083C7),
+          ),
+          child: Row(
+            children: [
+              Text(
+                "Performed",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+        );
+        break;
+      case QuestItemPriorityType.Starred:
+        returnWidget = SizedBox(height: 16,);
+        break;
+    }
+
+
+    return returnWidget;
+  }
+}
