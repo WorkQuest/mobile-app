93f2dca01731c245e1f9e4264acc3ee6b7c3849b
bakhanetssimon <Bakhanetssimon@gmail.com>
2021-05-21 16:14:35 +0700

    injector.config.dart updated

--- lib/di/injector.config.dart
+++ lib/di/injector.config.dart
@@ -7,12 +7,14 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../http/api_provider.dart' as _i7;
-import '../http/core/http_client.dart' as _i4;
-import '../http/core/i_http_client.dart' as _i3;
-import '../log_service.dart' as _i5;
-import '../ui/pages/sign_in_page/store/sign_in_store.dart' as _i6;
-import '../ui/pages/sign_up_page/store/sign_up_store.dart' as _i8;
+import '../http/api_provider.dart' as _i8;
+import '../http/core/http_client.dart' as _i5;
+import '../http/core/i_http_client.dart' as _i4;
+import '../log_service.dart' as _i6;
+import '../ui/pages/sign_in_page/store/sign_in_store.dart' as _i7;
+import '../ui/pages/sign_up_page/choose_role_page/store/choose_role_store.dart'
+    as _i3;
+import '../ui/pages/sign_up_page/store/sign_up_store.dart' as _i9;
 
 const String _test = 'test';
 const String _dev = 'dev';
@@ -26,15 +28,16 @@ extension GetItInjectableX on _i1.GetIt {
   _i1.GetIt init(
       {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
     final gh = _i2.GetItHelper(this, environment, environmentFilter);
-    gh.factory<_i3.IHttpClient>(() => _i4.TestHttpClient(),
+    gh.factory<_i3.ChooseRoleStore>(() => _i3.ChooseRoleStore());
+    gh.factory<_i4.IHttpClient>(() => _i5.TestHttpClient(),
         registerFor: {_test});
-    gh.factory<_i5.LogService>(() => _i5.LogServiceDev(),
+    gh.factory<_i6.LogService>(() => _i6.LogServiceDev(),
         registerFor: {_dev, _test});
-    gh.factory<_i5.LogService>(() => _i5.LogServiceProd(),
+    gh.factory<_i6.LogService>(() => _i6.LogServiceProd(),
         registerFor: {_prod});
-    gh.factory<_i6.SignInStore>(() => _i6.SignInStore(get<_i7.ApiProvider>()));
-    gh.factory<_i8.SignUpStore>(() => _i8.SignUpStore(get<_i7.ApiProvider>()));
-    gh.singleton<_i7.ApiProvider>(_i7.ApiProvider(get<_i3.IHttpClient>()));
+    gh.factory<_i7.SignInStore>(() => _i7.SignInStore(get<_i8.ApiProvider>()));
+    gh.factory<_i9.SignUpStore>(() => _i9.SignUpStore(get<_i8.ApiProvider>()));
+    gh.singleton<_i8.ApiProvider>(_i8.ApiProvider(get<_i4.IHttpClient>()));
     return this;
   }
 }
