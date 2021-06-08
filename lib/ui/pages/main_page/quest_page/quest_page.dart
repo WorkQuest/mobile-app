fb8b242fa03844c0f9368e2a7c76ea4f600045c4
bakhanetssimon <Bakhanetssimon@gmail.com>
2021-06-08 14:43:47 +0700

    quest_page.dart switch between map and list done

--- lib/ui/pages/main_page/quest_page/quest_page.dart
+++ lib/ui/pages/main_page/quest_page/quest_page.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
 
 import 'package:app/log_service.dart';
+import 'package:app/ui/pages/main_page/create_quest_page/create_quest_page.dart';
+import 'package:app/ui/pages/main_page/quest_page/store/quests_store.dart';
+import 'package:app/ui/pages/singleton_stores/profile_me_store.dart';
 import 'package:app/work_quest_app.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import "package:provider/provider.dart";
 
 class QuestPage extends StatefulWidget {
   @override
@@ -17,66 +22,112 @@ class _QuestPageState extends State<QuestPage> {
 
   late GoogleMapController _controller;
   CameraPosition? _initialCameraPosition;
+  QuestsStore? questsStore;
+  ProfileMeStore? profileMeStore;
 
   @override
   void initState() {
+    questsStore = context.read<QuestsStore>();
+    profileMeStore = context.read<ProfileMeStore>();
+    profileMeStore!.getProfileMe();
+    questsStore!.getQuests();
     _getCurrentLocation();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _initialCameraPosition == null
-          ? SizedBox()
-          : GoogleMap(
-              mapType: MapType.normal,
-              rotateGesturesEnabled: false,
-              initialCameraPosition: _initialCameraPosition!,
-              myLocationEnabled: true,
-              myLocationButtonEnabled: false,
-              onMapCreated: (GoogleMapController controller) {
-                _controller = controller;
-              },
-            ),
-      floatingActionButton: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 32.0),
-            child: Container(
-              // onPressed: _onMyLocationPressed,
-              padding: const EdgeInsets.only(
-                  top: 11, bottom: 11, left: 17, right: 15),
-              decoration: BoxDecoration(
-                color: AppColors.primary,
-                borderRadius: BorderRadius.circular(44),
-              ),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.map_outlined,
-                    color: Colors.white,
-                  ),
-                  SizedBox(
-                    width: 12,
-                  ),
-                  Text(
-                    "Map",
-                    style: TextStyle(color: Colors.white),
+    return Observer(
+      builder: (_) => Scaffold(
+        body: _initialCameraPosition == null
+            ? getBody()
+            : questsStore!.isMapOpened()
+                ? GoogleMap(
+                    mapType: MapType.normal,
+                    rotateGesturesEnabled: false,
+                    initialCameraPosition: _initialCameraPosition!,
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: false,
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller = controller;
+                    },
                   )
-                ],
+                : getBody(),
+        floatingActionButton: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 32.0),
+              child: GestureDetector(
+                onTap: () {
+                  print("value should be changed");
+                  setState(() {
+                    questsStore!.changeValue();
+                  });
+                },
+                child: Container(
+                  // onPressed: _onMyLocationPressed,
+                  padding: const EdgeInsets.only(
+                      top: 11, bottom: 11, left: 17, right: 15),
+                  decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(44),
+                  ),
+                  child: Row(
+                    children: [
+                      questsStore!.isMapOpened()
+                          ? Icon(
+                              Icons.list,
+                              color: Colors.white,
+                            )
+                          : Icon(
+                              Icons.map_outlined,
+                              color: Colors.white,
+                            ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      questsStore!.isMapOpened()
+                          ? Text(
+                              "List",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          : Text(
+                              "Map",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-          Spacer(),
-          FloatingActionButton(
-            onPressed: _onMyLocationPressed,
-            child: Icon(
-              Icons.location_on,
-            ),
-          ),
-        ],
+            Spacer(),
+            questsStore!.isMapOpened()
+                ? FloatingActionButton(
+                    onPressed: _onMyLocationPressed,
+                    child: Icon(
+                      Icons.location_on,
+                    ),
+                  )
+                : Container(),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget getBody() {
+    return Column(
+      children: [
+        SizedBox(
+          height: 250,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, CreateQuestPage.routeName);
+          },
+          child: Text("Create quest"),
+        ),
+      ],
     );
   }
 
