a9451434200570cb839f62381ee95cf907e8f4d8
bakhanetssimon <Bakhanetssimon@gmail.com>
2021-06-08 14:41:52 +0700

    profile_me_response.dart added

---
+++ lib/model/profile_me_response.dart
@@ -0,0 +1,32 @@
+class ProfileMeResponse {
+  ProfileMeResponse({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.role,
+  });
+
+  String id;
+  String firstName;
+  String lastName;
+  String email;
+  String role;
+
+  factory ProfileMeResponse.fromJson(Map<String, dynamic> json) =>
+      ProfileMeResponse(
+        id: json["id"],
+        firstName: json["firstName"],
+        lastName: json["lastName"],
+        email: json["email"],
+        role: json["role"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "firstName": firstName,
+        "lastName": lastName,
+        "email": email,
+        "role": role,
+      };
+}
