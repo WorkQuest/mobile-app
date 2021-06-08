75e24048349f714e7c8c836dbfb1d7ff4760a295
bakhanetssimon <Bakhanetssimon@gmail.com>
2021-06-08 14:41:23 +0700

    quests_store.dart created and logic integrated in store

---
+++ lib/ui/pages/main_page/quest_page/store/quests_store.dart
@@ -0,0 +1,55 @@
+import 'package:app/base_store/i_store.dart';
+import 'package:app/http/api_provider.dart';
+import 'package:app/model/quests_models/QuestsResponse.dart';
+import 'package:injectable/injectable.dart';
+import 'package:mobx/mobx.dart';
+
+part 'quests_store.g.dart';
+
+@injectable
+@singleton
+class QuestsStore extends _QuestsStore with _$QuestsStore {
+  QuestsStore(ApiProvider apiProvider) : super(apiProvider);
+}
+
+abstract class _QuestsStore extends IStore<bool> with Store {
+  final ApiProvider _apiProvider;
+
+  _QuestsStore(this._apiProvider);
+
+  @observable
+  List<QuestsResponse>? questsList;
+
+  @observable
+  _MapList mapListChecker = _MapList.Map;
+
+  @action
+  changeValue() {
+    if (mapListChecker == _MapList.Map) {
+      mapListChecker = _MapList.List;
+    } else {
+      mapListChecker = _MapList.Map;
+    }
+  }
+
+  @action
+  isMapOpened(){
+    return mapListChecker == _MapList.Map;
+  }
+
+  @action
+  Future getQuests() async {
+    try {
+      this.onLoading();
+      questsList = await _apiProvider.getQuests();
+      this.onSuccess(true);
+    } catch (e) {
+      this.onError(e.toString());
+    }
+  }
+}
+
+enum _MapList {
+  Map,
+  List,
+}
---
+++ lib/ui/pages/main_page/quest_page/store/quests_store.g.dart
@@ -0,0 +1,80 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'quests_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$QuestsStore on _QuestsStore, Store {
+  final _$questsListAtom = Atom(name: '_QuestsStore.questsList');
+
+  @override
+  List<QuestsResponse>? get questsList {
+    _$questsListAtom.reportRead();
+    return super.questsList;
+  }
+
+  @override
+  set questsList(List<QuestsResponse>? value) {
+    _$questsListAtom.reportWrite(value, super.questsList, () {
+      super.questsList = value;
+    });
+  }
+
+  final _$mapListCheckerAtom = Atom(name: '_QuestsStore.mapListChecker');
+
+  @override
+  _MapList get mapListChecker {
+    _$mapListCheckerAtom.reportRead();
+    return super.mapListChecker;
+  }
+
+  @override
+  set mapListChecker(_MapList value) {
+    _$mapListCheckerAtom.reportWrite(value, super.mapListChecker, () {
+      super.mapListChecker = value;
+    });
+  }
+
+  final _$getQuestsAsyncAction = AsyncAction('_QuestsStore.getQuests');
+
+  @override
+  Future<dynamic> getQuests() {
+    return _$getQuestsAsyncAction.run(() => super.getQuests());
+  }
+
+  final _$_QuestsStoreActionController = ActionController(name: '_QuestsStore');
+
+  @override
+  dynamic changeValue() {
+    final _$actionInfo = _$_QuestsStoreActionController.startAction(
+        name: '_QuestsStore.changeValue');
+    try {
+      return super.changeValue();
+    } finally {
+      _$_QuestsStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic isMapOpened() {
+    final _$actionInfo = _$_QuestsStoreActionController.startAction(
+        name: '_QuestsStore.isMapOpened');
+    try {
+      return super.isMapOpened();
+    } finally {
+      _$_QuestsStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+questsList: ${questsList},
+mapListChecker: ${mapListChecker}
+    ''';
+  }
+}
