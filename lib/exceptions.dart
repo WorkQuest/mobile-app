cfc311ca188c8a8c7bca3808e67aebc587efe08b
dayveed <dayveed.d@gmail.com>
2021-07-09 13:32:10 +0700

    updated error class

--- lib/exceptions.dart
+++ lib/exceptions.dart
@@ -1,10 +1,23 @@
-class RequestError implements Exception {
+class RequestErrorModel implements Exception {
   final String message;
+  final int errorCode;
 
-  const RequestError({required this.message});
+  const RequestErrorModel({required this.message, required this.errorCode});
+
+  @override
+  factory RequestErrorModel.fromJson(Map<String, dynamic> json) => RequestErrorModel(
+    errorCode: json["code"],
+    message: json["msg"],
+  );
 
   @override
   String toString() {
     return this.message;
   }
+
+  int returnErrorCode(){
+    return this.errorCode;
+  }
+
 }
+
