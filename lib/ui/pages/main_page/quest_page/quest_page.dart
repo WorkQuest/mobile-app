c0b86945f9befdae22162492ef13826bbbf17ca2
Roman <roman@MacBook-Pro-Roman.local>
2021-07-23 15:35:29 +0700

    Added display of the map in the detailed information of the quest, Changed the loading of markers and their size.

--- lib/ui/pages/main_page/quest_page/quest_page.dart
+++ lib/ui/pages/main_page/quest_page/quest_page.dart
@@ -36,7 +36,7 @@ class _QuestPageState extends State<QuestPage> {
     profileMeStore = context.read<ProfileMeStore>();
     profileMeStore!.getProfileMe();
     questsStore!.getQuests();
-    questsStore!.loadIcons();
+    questsStore!.loadIcons(context);
     _getCurrentLocation();
     super.initState();
   }
@@ -217,7 +217,7 @@ class _QuestPageState extends State<QuestPage> {
                 itemBuilder: (_, index) {
                   return MyQuestsItem(
                     questsStore!.questsList![index],
-                    this.questItemPriorityType,
+                    itemType: this.questItemPriorityType,
                   );
                 },
               ),
