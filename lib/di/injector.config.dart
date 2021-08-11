3747b2b150a4ece712768084c46f8b64f42faf69
dayveed <dayveed.d@gmail.com>
2021-08-11 11:54:04 +0700

    injector.config.dart updated

--- lib/di/injector.config.dart
+++ lib/di/injector.config.dart
@@ -13,14 +13,19 @@ import '../http/core/i_http_client.dart' as _i3;
 import '../log_service.dart' as _i5;
 import '../ui/pages/main_page/create_quest_page/store/create_quest_store.dart'
     as _i9;
-import '../ui/pages/main_page/quest_page/store/quests_store.dart' as _i10;
-import '../ui/pages/main_page/settings_page/store/settings_store.dart' as _i11;
-import '../ui/pages/pin_code_page/store/pin_code_store.dart' as _i14;
-import '../ui/pages/profile_me_store/profile_me_store.dart' as _i15;
-import '../ui/pages/sign_in_page/store/sign_in_store.dart' as _i12;
+import '../ui/pages/main_page/quest_page/store/quests_store.dart' as _i11;
+import '../ui/pages/main_page/settings_page/store/settings_store.dart' as _i12;
+import '../ui/pages/main_page/wallet_page/deposit_page/store/deposit_store.dart'
+    as _i10;
+import '../ui/pages/main_page/wallet_page/store/wallet_store.dart' as _i15;
+import '../ui/pages/main_page/wallet_page/withdraw_page/store/withdraw_page_store.dart'
+    as _i16;
+import '../ui/pages/pin_code_page/store/pin_code_store.dart' as _i17;
+import '../ui/pages/profile_me_store/profile_me_store.dart' as _i18;
+import '../ui/pages/sign_in_page/store/sign_in_store.dart' as _i13;
 import '../ui/pages/sign_up_page/choose_role_page/store/choose_role_store.dart'
     as _i7;
-import '../ui/pages/sign_up_page/store/sign_up_store.dart' as _i13;
+import '../ui/pages/sign_up_page/store/sign_up_store.dart' as _i14;
 import '../utils/set_environment.dart' as _i6;
 
 const String _test = 'test';
@@ -46,18 +51,24 @@ extension GetItInjectableX on _i1.GetIt {
         () => _i7.ChooseRoleStore(get<_i8.ApiProvider>()));
     gh.factory<_i9.CreateQuestStore>(
         () => _i9.CreateQuestStore(get<_i8.ApiProvider>()));
-    gh.factory<_i10.QuestsStore>(
-        () => _i10.QuestsStore(get<_i8.ApiProvider>()));
-    gh.factory<_i11.SettingsPageStore>(
-        () => _i11.SettingsPageStore(get<_i8.ApiProvider>()));
-    gh.factory<_i12.SignInStore>(
-        () => _i12.SignInStore(get<_i8.ApiProvider>()));
-    gh.factory<_i13.SignUpStore>(
-        () => _i13.SignUpStore(get<_i8.ApiProvider>()));
+    gh.factory<_i10.DepositStore>(
+        () => _i10.DepositStore(get<_i8.ApiProvider>()));
+    gh.factory<_i11.QuestsStore>(
+        () => _i11.QuestsStore(get<_i8.ApiProvider>()));
+    gh.factory<_i12.SettingsPageStore>(
+        () => _i12.SettingsPageStore(get<_i8.ApiProvider>()));
+    gh.factory<_i13.SignInStore>(
+        () => _i13.SignInStore(get<_i8.ApiProvider>()));
+    gh.factory<_i14.SignUpStore>(
+        () => _i14.SignUpStore(get<_i8.ApiProvider>()));
+    gh.factory<_i15.WalletStore>(
+        () => _i15.WalletStore(get<_i8.ApiProvider>()));
+    gh.factory<_i16.WithdrawPageStore>(
+        () => _i16.WithdrawPageStore(get<_i8.ApiProvider>()));
     gh.singleton<_i8.ApiProvider>(_i8.ApiProvider(get<_i3.IHttpClient>()));
-    gh.singleton<_i14.PinCodeStore>(_i14.PinCodeStore(get<_i8.ApiProvider>()));
-    gh.singleton<_i15.ProfileMeStore>(
-        _i15.ProfileMeStore(get<_i8.ApiProvider>()));
+    gh.singleton<_i17.PinCodeStore>(_i17.PinCodeStore(get<_i8.ApiProvider>()));
+    gh.singleton<_i18.ProfileMeStore>(
+        _i18.ProfileMeStore(get<_i8.ApiProvider>()));
     return this;
   }
 }
