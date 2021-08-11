47be1ac40c43abb558172ecd4898dacd14dff45e
dayveed <dayveed.d@gmail.com>
2021-08-11 11:48:16 +0700

    withdraw_page.dart added with store

---
+++ lib/ui/pages/main_page/wallet_page/withdraw_page/store/withdraw_page_store.dart
@@ -0,0 +1,42 @@
+import 'package:app/http/api_provider.dart';
+import 'package:injectable/injectable.dart';
+import 'package:app/base_store/i_store.dart';
+import 'package:mobx/mobx.dart';
+
+part 'withdraw_page_store.g.dart';
+
+@injectable
+class WithdrawPageStore extends _WithdrawPageStore with _$WithdrawPageStore {
+  WithdrawPageStore(ApiProvider apiProvider) : super(apiProvider);
+}
+
+abstract class _WithdrawPageStore extends IStore<bool> with Store {
+  final ApiProvider apiProvider;
+
+  _WithdrawPageStore(this.apiProvider);
+
+  @observable
+  String _recipientAddress = '';
+  @observable
+  String _amount = '';
+
+  @action
+  void setRecipientAddress(String value) {
+    _recipientAddress = value;
+  }
+
+  @action
+  void setAmount(String value) {
+    _amount = value;
+  }
+
+  @action
+  String getAmount() => _amount;
+
+  @action
+  String getAddress() => _recipientAddress;
+
+  @computed
+  bool get canSubmit =>
+      !isLoading && _recipientAddress.isNotEmpty && _amount.isNotEmpty;
+}
