29d202861b00238dc7619fa62a8024a169bcc98b
dayveed <dayveed.d@gmail.com>
2021-08-19 14:45:27 +0700

    main.dart updated for notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,63 @@
+import 'package:app/utils/push_notification_service.dart';
 import 'package:app/utils/storage.dart';
 import 'package:app/work_quest_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:injectable/injectable.dart';
 import 'di/injector.dart';
 import 'package:easy_localization/easy_localization.dart';
 
+///Android Notification Channel
+
+const AndroidNotificationChannel _channel = AndroidNotificationChannel(
+  'high_importance_channel',
+  'High Importance Notification',
+  'This channel is used for important notifications',
+  importance: Importance.max,
+  playSound: true,
+);
+
+final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+///BackGround Message Handler
+
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   await Firebase.initializeApp();
+//   print("Handling a background message: ${message.messageId}");
+// }
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //_initialisePushNotification();
   injectDependencies(env: Environment.test);
+  await Firebase.initializeApp().then(
+        (value) => _initialisePushNotification(),
+  );
   await EasyLocalization.ensureInitialized();
   runApp(
     EasyLocalization(
-        child: WorkQuestApp(await Storage.toLoginCheck()),
-        supportedLocales: [Locale('en', 'US'), Locale('ru', 'RU'), Locale('ar','SA')],
-        path: 'assets/lang',
-        ),
+      child: WorkQuestApp(await Storage.toLoginCheck()),
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('ru', 'RU'),
+        Locale('ar', 'SA'),
+      ],
+      path: 'assets/lang',
+    ),
   );
+}
+
+void _initialisePushNotification() {
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+   PushNotificationService(
+    _firebaseMessaging,
+    _channel,
+    _flutterLocalNotificationsPlugin,
+  );
+  _firebaseMessaging.subscribeToTopic('all');
+  //firebaseMessaging.getToken().then((token) => print(" firebase token $token"));
+  //FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 }
