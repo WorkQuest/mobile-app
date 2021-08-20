6dcea872e2684b98bdd907b48d18f6130691d18f
dayveed <dayveed.d@gmail.com>
2021-08-20 11:25:12 +0700

    added observer consumer in sms_verification_page.dart

--- lib/exceptions.dart
+++ lib/exceptions.dart
@@ -2,22 +2,26 @@ class RequestErrorModel implements Exception {
   final String message;
   final int errorCode;
 
-  const RequestErrorModel({required this.message, required this.errorCode});
+  const RequestErrorModel({
+    required this.message,
+    required this.errorCode,
+  });
 
   @override
-  factory RequestErrorModel.fromJson(Map<String, dynamic> json) => RequestErrorModel(
-    errorCode: json["code"],
-    message: json["msg"],
-  );
+  factory RequestErrorModel.fromJson(
+    Map<String, dynamic> json,
+  ) =>
+      RequestErrorModel(
+        errorCode: json["code"],
+        message: json["msg"],
+      );
 
   @override
   String toString() {
     return this.message;
   }
 
-  int returnErrorCode(){
+  int returnErrorCode() {
     return this.errorCode;
   }
-
 }
-
--- lib/ui/pages/main_page/settings_page/pages/SMS_verification_page/sms_verification_page.dart
+++ lib/ui/pages/main_page/settings_page/pages/SMS_verification_page/sms_verification_page.dart
@@ -1,3 +1,4 @@
+import 'package:app/observer_consumer.dart';
 import 'package:app/ui/pages/main_page/settings_page/pages/SMS_verification_page/store/sms_verification_store.dart';
 import 'package:app/ui/widgets/platform_activity_indicator.dart';
 import 'package:flutter/cupertino.dart';
@@ -11,105 +12,108 @@ class SMSVerificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final store = context.read<SMSVerificationStore>();
-    return Observer(
-      builder: (_) => Scaffold(
-        appBar: CupertinoNavigationBar(
-          automaticallyImplyLeading: true,
-          middle: Text("SMS Verification"),
-        ),
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 16.0,
-            ),
-            child: IndexedStack(
-              index: store.index,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Phone"),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    TextFormField(
-                      maxLines: 1,
-                      initialValue: store.phone,
-                      onChanged: store.setPhone,
-                      keyboardType: TextInputType.phone,
-                      decoration: InputDecoration(
-                        hintText: "+7 *** *** ** **",
+    return ObserverListener<SMSVerificationStore>(
+      onSuccess: (){},
+      child: Observer(
+        builder: (_) => Scaffold(
+          appBar: CupertinoNavigationBar(
+            automaticallyImplyLeading: true,
+            middle: Text("SMS Verification"),
+          ),
+          body: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 16.0,
+              ),
+              child: IndexedStack(
+                index: store.index,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Phone"),
+                      const SizedBox(
+                        height: 10.0,
                       ),
-                    ),
-                    Spacer(),
-                    ElevatedButton(
-                      onPressed: store.phone.length < 7
-                          ? null
-                          : () async {
-                              await store.submitPhoneNumber();
-                              store.index = 1;
-                            },
-                      child: store.isLoading
-                          ? Center(
-                              child: PlatformActivityIndicator(),
-                            )
-                          : Text("Submit"),
-                    ),
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Code"),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                    TextFormField(
-                      maxLines: 1,
-                      onChanged: store.setCode,
-                      keyboardType: TextInputType.phone,
-                      decoration: InputDecoration(
-                        hintText: "Code",
+                      TextFormField(
+                        maxLines: 1,
+                        initialValue: store.phone,
+                        onChanged: store.setPhone,
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          hintText: "+7 *** *** ** **",
+                        ),
                       ),
-                    ),
-                    Spacer(),
-                    Row(
-                      children: [
-                        OutlinedButton(
-                          onPressed: () => store.index = 0,
-                          child: Text("Back"),
-                          style: OutlinedButton.styleFrom(
-                            side: BorderSide(
-                              width: 1.0,
-                              color: Color(0xFF0083C7).withOpacity(0.1),
+                      Spacer(),
+                      ElevatedButton(
+                        onPressed: store.phone.length < 7
+                            ? null
+                            : () async {
+                                await store.submitPhoneNumber();
+                                store.index = 1;
+                              },
+                        child: store.isLoading
+                            ? Center(
+                                child: PlatformActivityIndicator(),
+                              )
+                            : Text("Submit"),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Code"),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      TextFormField(
+                        maxLines: 1,
+                        onChanged: store.setCode,
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          hintText: "Code",
+                        ),
+                      ),
+                      Spacer(),
+                      Row(
+                        children: [
+                          OutlinedButton(
+                            onPressed: () => store.index = 0,
+                            child: Text("Back"),
+                            style: OutlinedButton.styleFrom(
+                              side: BorderSide(
+                                width: 1.0,
+                                color: Color(0xFF0083C7).withOpacity(0.1),
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(
-                          width: 10.0,
-                        ),
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: store.code.length < 4
-                                ? null
-                                : () async {
-                                    await store.submitCode();
-                                    if (store.isSuccess)
-                                      Navigator.of(context).pop();
-                                  },
-                            child: store.isLoading
-                                ? Center(
-                                    child: PlatformActivityIndicator(),
-                                  )
-                                : Text("Send Code"),
+                          const SizedBox(
+                            width: 10.0,
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: store.code.length < 4
+                                  ? null
+                                  : () async {
+                                      await store.submitCode();
+                                      if (store.isSuccess)
+                                        Navigator.pop(context);
+                                    },
+                              child: store.isLoading
+                                  ? Center(
+                                      child: PlatformActivityIndicator(),
+                                    )
+                                  : Text("Send Code"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
