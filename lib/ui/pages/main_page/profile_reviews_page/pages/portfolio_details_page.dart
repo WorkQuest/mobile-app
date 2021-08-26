057bd4820368b46719823947d1d6d85a496d7faa
dayveed <dayveed.d@gmail.com>
2021-08-26 13:40:17 +0700

    portfolio_details_page.dart added

---
+++ lib/ui/pages/main_page/profile_reviews_page/pages/portfolio_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class PortfolioDetails extends StatelessWidget {
+  const PortfolioDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
