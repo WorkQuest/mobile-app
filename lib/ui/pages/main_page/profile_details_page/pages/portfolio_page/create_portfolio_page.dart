0374461a0336f18230de0bfaee2bd4b516655739
dayveed <dayveed.d@gmail.com>
2021-09-06 14:30:30 +0700

    updated create_portfolio_page.dart

--- lib/ui/pages/main_page/profile_details_page/pages/portfolio_page/create_portfolio_page.dart
+++ lib/ui/pages/main_page/profile_details_page/pages/portfolio_page/create_portfolio_page.dart
@@ -4,6 +4,7 @@ import 'package:app/ui/widgets/media_upload_widget.dart';
 import 'package:app/ui/widgets/platform_activity_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import "package:provider/provider.dart";
 
@@ -40,6 +41,73 @@ class CreatePortfolioPage extends StatelessWidget {
                 ),
                 sliver: SliverList(
                   delegate: SliverChildListDelegate([
+                    ///media view on edit portfolio
+                    if(allowEdit)
+                    SizedBox(
+                      height:150.0,
+                      child: ListView.separated(
+                        separatorBuilder: (_,index)=>SizedBox(
+                          width: 10.0,
+                        ),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount:store.portfolioList[store.portfolioIndex]
+                            .medias.length,
+                        itemBuilder: (_, index) => SizedBox(
+                          width: 120.0,
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            fit: StackFit.expand,
+                            children: [
+                              // Media
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: Image.network(
+                                  store.portfolioList[store.portfolioIndex]
+                                      .medias[index].url,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Positioned(
+                                top: -15.0,
+                                 right: -15.0,
+                                child: IconButton(
+                                  onPressed: () => store.portfolioList[store.portfolioIndex]
+                                      .medias.removeAt(index),
+                                  //widget.media.removeAt(index),
+                                  icon: Icon(Icons.cancel),
+                                  color: Colors.redAccent,
+                                ),
+                              ),
+                              // For video duration
+                              // if (widget.media[index].entity.type == AssetType.video)
+                              //   Positioned(
+                              //     right: 4.0,
+                              //     bottom: 4.0,
+                              //     child: ClipRRect(
+                              //       borderRadius: BorderRadius.circular(20.0),
+                              //       child: Container(
+                              //         color: Colors.black.withOpacity(0.7),
+                              //         padding: const EdgeInsets.symmetric(
+                              //             horizontal: 6.0, vertical: 2.0),
+                              //         child: Text(
+                              //           widget.media[index].entity.duration.formattedDuration,
+                              //           textAlign: TextAlign.center,
+                              //           style: const TextStyle(
+                              //             fontSize: 13.0,
+                              //             color: Colors.white,
+                              //             fontWeight: FontWeight.w700,
+                              //           ),
+                              //         ),
+                              //       ),
+                              //     ),
+                              //   ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    _spacer,
                     const Text("Title"),
                     _spacer,
                     TextFormField(
@@ -74,7 +142,9 @@ class CreatePortfolioPage extends StatelessWidget {
                     ),
                     const Text("Files"),
                     _spacer,
-                    MediaUpload(media: store.media)
+                    MediaUpload(
+                      media: store.media,
+                    )
                   ]),
                 ),
               ),
@@ -83,6 +153,7 @@ class CreatePortfolioPage extends StatelessWidget {
                 child: Padding(
                   padding: EdgeInsets.symmetric(
                     horizontal: 15.0,
+                    vertical: 20.0,
                   ),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
@@ -90,7 +161,10 @@ class CreatePortfolioPage extends StatelessWidget {
                     children: <Widget>[
                       Expanded(
                         child: ObserverListener<PortfolioStore>(
-                          onSuccess: () => Navigator.pop(context),
+                          onSuccess: () {
+                            Navigator.pop(context);
+                            store.media.clear();
+                          },
                           child: ElevatedButton(
                             onPressed: store.canSubmit
                                 ? () async {
