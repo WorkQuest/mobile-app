4daa6e8237343c90d6f78713d549fd19ecf2bf4f
dayveed <dayveed.d@gmail.com>
2021-12-20 12:00:55 +0700

    fix on scroll, refactor on quest list widget

--- lib/ui/pages/main_page/my_quests_page/quests_list.dart
+++ lib/ui/pages/main_page/my_quests_page/quests_list.dart
@@ -1,5 +1,4 @@
 import 'package:app/model/quests_models/base_quest_response.dart';
-import 'package:app/ui/pages/main_page/quest_page/create_quest_page/create_quest_page.dart';
 import 'package:app/ui/widgets/platform_activity_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
@@ -13,8 +12,6 @@ import 'my_quests_item.dart';
 class QuestsList extends StatelessWidget {
   final QuestItemPriorityType questItemPriorityType;
 
-  final Function(bool)? onCreate;
-
   ObservableList<BaseQuestResponse> questsList = ObservableList.of([]);
 
   final Future<dynamic>? update;
@@ -24,7 +21,7 @@ class QuestsList extends StatelessWidget {
   final bool isLoading;
 
   QuestsList(this.questItemPriorityType, this.questsList,
-      {this.onCreate,
+      {
       this.update,
       this.physics = const BouncingScrollPhysics(
         parent: AlwaysScrollableScrollPhysics(),
@@ -34,8 +31,7 @@ class QuestsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Observer(
-      builder: (_) => Container(
-        color: const Color(0xFFF7F8FA),
+      builder: (_) => Center(
         child: questsList.isEmpty
             ? isLoading
                 ? getLoadingBody()
@@ -51,36 +47,11 @@ class QuestsList extends StatelessWidget {
       child: ListView.builder(
         physics: physics,
         shrinkWrap: true,
-        itemCount: onCreate != null ? questsList.length + 1 : questsList.length,
+        itemCount: questsList.length,
         padding: EdgeInsets.zero,
         itemBuilder: (BuildContext context, index) {
-          if (onCreate != null) if (index == 0) {
-            return Container(
-              margin: EdgeInsets.only(top: 10.0),
-              color: Colors.white,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        bool? status =
-                            await Navigator.of(context, rootNavigator: true)
-                                .pushNamed<bool>(CreateQuestPage.routeName);
-                        onCreate!(status ?? false);
-                      },
-                      child: Text(
-                        "quests.addNewQuest".tr(),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          }
           return MyQuestsItem(
-            questsList[(onCreate != null) ? index - 1 : index],
+            questsList[index],
             itemType: questItemPriorityType,
           );
         },
@@ -91,41 +62,21 @@ class QuestsList extends StatelessWidget {
   Widget getEmptyBody(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        if (onCreate != null)
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context, rootNavigator: true)
-                    .pushNamed(CreateQuestPage.routeName);
-              },
-              child: Text(
-                "quests.addNewQuest".tr(),
-              ),
-            ),
-          ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.only(top: 20.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  "assets/empty_quest_icon.svg",
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  "quests.youDontHaveAny".tr() +
-                      " ${questItemPriorityType.toString().split(".").last} " +
-                      "quests.questYet".tr(),
-                ),
-              ],
-            ),
+        SvgPicture.asset(
+          "assets/empty_quest_icon.svg",
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          "quests.youDontHaveAny".tr() +
+              " ${questItemPriorityType.toString().split(".").last} " +
+              "quests.questYet".tr(),
+          style: TextStyle(
+            color:Color(0xFFD8DFE3),
           ),
         ),
       ],
