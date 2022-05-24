59b4da38c865b1cf55595060d0b15ae7a8d6907e
Alexander <48985323+Dogayo@users.noreply.github.com>
2022-05-24 15:02:53 +0700

    Show local default avatar by error getting url

--- lib/ui/widgets/user_avatar.dart
+++ lib/ui/widgets/user_avatar.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:app/ui/widgets/gradient_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../constants.dart';
 
@@ -19,12 +21,30 @@ class UserAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // return Container(
+    //   width: width,
+    //   height: height,
+    //   decoration: BoxDecoration(
+    //     shape: BoxShape.circle,
+    //   ),
+    //   child: Image.asset('assets/wq_default_avatar.png'),
+    // );
     return FadeInImage.memoryNetwork(
       width: width,
       height: height,
       fit: BoxFit.cover,
       fadeOutDuration: const Duration(milliseconds: 250),
       image: url ?? Constants.defaultImageNetwork,
+      imageErrorBuilder: (context, error, _) {
+        return Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+          ),
+          child: Image.asset('assets/wq_default_avatar.png'),
+        );
+      },
       placeholder: Uint8List.fromList(
         base64Decode(Constants.base64WhiteHolder),
       ),
