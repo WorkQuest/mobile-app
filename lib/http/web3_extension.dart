7cc0665dae3844554aea3740d2285826cc56e5d2
Alexander <48985323+Dogayo@users.noreply.github.com>
2022-06-17 11:02:06 +0700

    Add model ang request for Course WQT

--- lib/http/web3_extension.dart
+++ lib/http/web3_extension.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:app/constants.dart';
 import 'package:app/model/web3/transactions_response.dart';
 import 'package:app/web3/repository/account_repository.dart';
 
+import '../model/web3/course_token_response.dart';
 import 'api_provider.dart';
 
 extension Web3Requests on ApiProvider {
@@ -16,7 +19,6 @@ extension Web3Requests on ApiProvider {
     }
   }
 
-
   String _transactions(String address) {
     if (AccountRepository().configName == ConfigNameNetwork.testnet) {
       return "https://testnet-explorer-api.workquest.co/api/v1/account/$address/transactions";
@@ -24,7 +26,6 @@ extension Web3Requests on ApiProvider {
       return "https://dev-explorer.workquest.co/api/v1/account/$address/transactions";
     }
   }
-
 
   Future<void> registerWallet(String publicKey, String address) async {
     await httpClient.post(
@@ -36,10 +37,20 @@ extension Web3Requests on ApiProvider {
     );
   }
 
-  Future<List<Tx>?> getTransactions(String address,
-      {int limit = 10, int offset = 0}) async {
-    try {
+  Future<double> getCourseWQT() async {
+    final response = await httpClient.get(
+      query: "https://dev-oracle.workquest.co/api/v1/oracle/sign-price/tokens",
+      useBaseUrl: false,
+    );
 
+    final result = CourseTokenResponse.fromJson(response);
+    final _indexWQT = result.symbols!.indexWhere((element) => element == 'WQT');
+    final _course = result.prices![_indexWQT];
+    return double.parse(_course) * pow(10, -18);
+  }
+
+  Future<List<Tx>?> getTransactions(String address, {int limit = 10, int offset = 0}) async {
+    try {
       final response = await httpClient.get(
         query: '${_transactions(address)}?limit=$limit&offset=$offset',
         useBaseUrl: false,
---
+++ lib/model/web3/course_token_response.dart
@@ -0,0 +1,45 @@
+// To parse this JSON data, do
+//
+//     final courseTokenReponse = courseTokenReponseFromJson(jsonString);
+
+import 'dart:convert';
+
+CourseTokenResponse courseTokenResponseFromJson(String str) => CourseTokenResponse.fromJson(json.decode(str));
+
+String courseTokenResponseToJson(CourseTokenResponse data) => json.encode(data.toJson());
+
+class CourseTokenResponse {
+  CourseTokenResponse({
+    this.nonce,
+    this.prices,
+    this.v,
+    this.r,
+    this.s,
+    this.symbols,
+  });
+
+  int? nonce;
+  List<String> ?prices;
+  String? v;
+  String? r;
+  String? s;
+  List<String>? symbols;
+
+  factory CourseTokenResponse.fromJson(Map<String, dynamic> json) => CourseTokenResponse(
+    nonce: json["nonce"],
+    prices: json["prices"] == null ? null : List<String>.from(json["prices"].map((x) => x)),
+    v: json["v"],
+    r: json["r"],
+    s: json["s"],
+    symbols: json["symbols"] == null ? null : List<String>.from(json["symbols"].map((x) => x)),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "nonce": nonce,
+    "prices": prices == null ? null : List<dynamic>.from(prices!.map((x) => x)),
+    "v": v,
+    "r": r,
+    "s": s,
+    "symbols": symbols == null ? null : List<dynamic>.from(symbols!.map((x) => x)),
+  };
+}
