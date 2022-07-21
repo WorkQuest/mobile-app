abd9cd6d0aab8e83e09ee31f30a782c754355fef
Alexander <48985323+Dogayo@users.noreply.github.com>
2022-07-21 11:38:56 +0700

    Refactor create private chat

--- lib/ui/pages/main_page/chat_page/chat_room_page/create_private_chat/create_private_page.dart
+++ lib/ui/pages/main_page/chat_page/chat_room_page/create_private_chat/create_private_page.dart
@@ -1,6 +1,8 @@
+import 'package:app/observer_consumer.dart';
 import 'package:app/ui/pages/main_page/chat_page/chat_room_page/chat_room_page.dart';
 import 'package:app/ui/pages/main_page/chat_page/chat_room_page/create_private_chat/store/create_private_store.dart';
 import 'package:app/ui/widgets/dismiss_keyboard.dart';
+import 'package:app/ui/widgets/login_button.dart';
 import 'package:app/ui/widgets/media_upload/media_upload_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
@@ -29,100 +31,101 @@ class _CreatePrivatePageState extends State<CreatePrivatePage> {
 
   @override
   Widget build(BuildContext context) {
-    return DismissKeyboard(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () => Navigator.pop(context),
-            icon: Icon(
-              Icons.arrow_back_ios_sharp,
+    return ObserverListener<CreatePrivateStore>(
+      onSuccess: () {
+        Navigator.of(context, rootNavigator: true).pushReplacementNamed(
+          ChatRoomPage.routeName,
+          arguments: ChatRoomArguments(store.chatId, true),
+        );
+      },
+      onFailure: () => false,
+      child: DismissKeyboard(
+        child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: Icon(
+                Icons.arrow_back_ios_sharp,
+              ),
+            ),
+            centerTitle: true,
+            title: Text(
+              "chat.privateMessage".tr(),
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
             ),
           ),
-          centerTitle: true,
-          title: Text(
-            "chat.privateMessage".tr(),
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-              color: Colors.black,
-            ),
-          ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: ListView(
-            children: [
-              const SizedBox(height: 23),
-              Text("chat.message".tr()),
-              const SizedBox(height: 10),
-              Container(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxHeight: 300.0),
-                  child: TextField(
-                    onChanged: store.setMessage,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: InputDecoration(hintText: "chat.typeMessage".tr()),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ListView(
+              children: [
+                const SizedBox(height: 23),
+                Text("chat.message".tr()),
+                const SizedBox(height: 10),
+                Container(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxHeight: 300.0),
+                    child: TextField(
+                      onChanged: store.setMessage,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: InputDecoration(hintText: "chat.typeMessage".tr()),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: MediaUploadWithProgress(
-                  store: store,
-                  type: MediaType.images,
+                const SizedBox(height: 15),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: MediaUploadWithProgress(
+                    store: store,
+                    type: MediaType.images,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 21),
-              Row(
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 45.0,
-                      child: OutlinedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text("meta.cancel".tr()),
-                        style: OutlinedButton.styleFrom(
-                          side: BorderSide(
-                            width: 1.0,
-                            color: Color(0xFF0083C7).withOpacity(0.1),
+                const SizedBox(height: 21),
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        height: 45.0,
+                        child: OutlinedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text("meta.cancel".tr()),
+                          style: OutlinedButton.styleFrom(
+                            side: BorderSide(
+                              width: 1.0,
+                              color: Color(0xFF0083C7).withOpacity(0.1),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 20.0),
-                  Expanded(
-                    child: Observer(
-                      builder: (_) => ElevatedButton(
-                        onPressed: () async {
-                          await store.sendMessage(widget.userId);
-                          if (store.isSuccess) {
-                            Navigator.of(context, rootNavigator: true)
-                                .pushReplacementNamed(
-                              ChatRoomPage.routeName,
-                              arguments: ChatRoomArguments(store.chatId, true),
-                            );
-                          }
-                        },
-                        child: store.isLoading
-                            ? Center(
-                                child: CircularProgressIndicator.adaptive(),
-                              )
-                            : Text("meta.send".tr()),
+                    const SizedBox(width: 20.0),
+                    Expanded(
+                      child: Observer(
+                        builder: (_) => LoginButton(
+                          enabled: store.isLoading,
+                          onTap: _onPressedSend,
+                          title: "meta.send".tr(),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 15),
-            ],
+                  ],
+                ),
+                const SizedBox(height: 15),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  _onPressedSend() {
+    store.sendMessage(widget.userId);
   }
 }
