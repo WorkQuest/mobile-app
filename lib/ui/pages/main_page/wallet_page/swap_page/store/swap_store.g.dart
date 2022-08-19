8fdaae4be38e20aede4e2d716ab26ce1cf65baf4
Alexander <48985323+Dogayo@users.noreply.github.com>
2022-08-19 15:31:17 +0700

    Set default network in swap page

--- lib/ui/pages/main_page/wallet_page/swap_page/store/swap_store.g.dart
+++ lib/ui/pages/main_page/wallet_page/swap_page/store/swap_store.g.dart
@@ -147,7 +147,7 @@ mixin _$SwapStore on SwapStoreBase, Store {
   final _$setNetworkAsyncAction = AsyncAction('SwapStoreBase.setNetwork');
 
   @override
-  Future setNetwork(SwapNetworks? value, {bool isForce = false}) {
+  Future setNetwork(SwapNetworks value, {bool isForce = false}) {
     return _$setNetworkAsyncAction
         .run(() => super.setNetwork(value, isForce: isForce));
   }
