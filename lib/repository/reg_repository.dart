48fe3a64d06570ee671f27e996dd402468727551
Alexander <48985323+Dogayo@users.noreply.github.com>
2022-08-25 19:54:29 +0700

    Add exception

--- lib/repository/reg_repository.dart
+++ lib/repository/reg_repository.dart
@@ -24,19 +24,33 @@ class RegRepository extends IRegRepository {
     required String lastName,
     required String password,
   }) async {
-    if (WalletRepository().networkName.value == null) {
-      final _networkName = WalletRepository().notifierNetwork.value == Network.mainnet
-          ? NetworkName.workNetMainnet
-          : NetworkName.workNetTestnet;
-      WalletRepository().setNetwork(_networkName);
+    try {
+      if (WalletRepository().networkName.value == null) {
+        final _networkName = WalletRepository().notifierNetwork.value == Network.mainnet
+            ? NetworkName.workNetMainnet
+            : NetworkName.workNetTestnet;
+        WalletRepository().setNetwork(_networkName);
+      }
+      final bearerToken = await _apiProvider.register(
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        password: password,
+      );
+      Storage.writeRefreshToken(bearerToken.refresh);
+      Storage.writeAccessToken(bearerToken.access);
+    } catch (e) {
+      print('RegRepository register | error: $e');
+      throw RegException(e.toString());
     }
-    final bearerToken = await _apiProvider.register(
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
-      password: password,
-    );
-    Storage.writeRefreshToken(bearerToken.refresh);
-    Storage.writeAccessToken(bearerToken.access);
   }
+}
+
+class RegException implements Exception{
+  final String message;
+
+  const RegException([this.message = 'Unknown registration error']);
+
+  @override
+  String toString() => message;
 }
